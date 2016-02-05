0e42ad5d8e7063905796f7e2edce7a34a6f08a5f
unional <homawong@gmail.com>
2016-02-04 20:28:32 -0800

    Relax complexity to 25

--- packages/eslint-config-unional/index.js
+++ packages/eslint-config-unional/index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: 'airbnb/base',
   rules: {
       // Best Practices
-      complexity: [2, 11],
+      complexity: [2, 25],
 
       // Stylistic Issues
 	  'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
